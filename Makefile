84fa398457763de1f53f81ea721131a1b19ee4bf
Soma Natarajan <soma.voyager@gmail.com>
2020-12-17 10:51:46 -0500

    Updated build numbers

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 DEFAULT_DOCKER_NET := game
-API_SERVER_IMAGE := gcr.io/voyager-01-285603/api-server:0.1.40
-GAME_SERVER_IMAGE := gcr.io/voyager-01-285603/game-server:0.1.18
-NATS_SERVER_IMAGE := gcr.io/voyager-01-285603/nats-server:0.1.18
+API_SERVER_IMAGE := gcr.io/voyager-01-285603/api-server:0.1.41
+GAME_SERVER_IMAGE := gcr.io/voyager-01-285603/game-server:0.1.20
+NATS_SERVER_IMAGE := gcr.io/voyager-01-285603/nats-server:0.1.20
 REDIS_IMAGE := gcr.io/voyager-01-285603/redis:6.0.9
 POSTGRES_IMAGE := gcr.io/voyager-01-285603/postgres:12.5
-BOTRUNNER_IMAGE := gcr.io/voyager-01-285603/botrunner:0.1.24
+BOTRUNNER_IMAGE := gcr.io/voyager-01-285603/botrunner:0.1.30
 
 .PHONY: pull
 pull: 
--- env.bat
+++ env.bat
@@ -1,10 +1,11 @@
 SET DEFAULT_DOCKER_NET=game
-SET API_SERVER_IMAGE=gcr.io/voyager-01-285603/api-server:0.1.37
-SET GAME_SERVER_IMAGE=gcr.io/voyager-01-285603/game-server:0.1.18
-SET NATS_SERVER_IMAGE=gcr.io/voyager-01-285603/nats-server:0.1.18
+SET API_SERVER_IMAGE=gcr.io/voyager-01-285603/api-server:0.1.41
+SET GAME_SERVER_IMAGE=gcr.io/voyager-01-285603/game-server:0.1.20
+SET BOTRUNNER_IMAGE=gcr.io/voyager-01-285603/botrunner:0.1.30
+
+SET NATS_SERVER_IMAGE=gcr.io/voyager-01-285603/nats-server:0.1.20
 SET REDIS_IMAGE=gcr.io/voyager-01-285603/redis:6.0.9
 SET POSTGRES_IMAGE=gcr.io/voyager-01-285603/postgres:12.5
-SET BOTRUNNER_IMAGE=gcr.io/voyager-01-285603/botrunner:0.1.24
 del .env
 echo API_SERVER_IMAGE=%API_SERVER_IMAGE% >> .env
 echo GAME_SERVER_IMAGE=%GAME_SERVER_IMAGE% >> .env
