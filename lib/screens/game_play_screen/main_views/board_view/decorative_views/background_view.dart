0ae64545ac593577465aa8dc18d5668b8e64607a
somavoyager <66895802+somavoyager@users.noreply.github.com>
2021-01-25 08:14:54 -0500

    Game screen enhancements (#95)
    * added table image, background image, added tilted perspective board view
    
    * Fixed bugs
    
    * Updated version numbers
    
    Co-authored-by: Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
    Co-authored-by: somavoyager <somavoyager@gmail.com>

---
+++ lib/screens/game_play_screen/main_views/board_view/decorative_views/background_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pokerapp/resources/app_assets.dart';
+
+class BackgroundView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      AppAssets.cityBackground1,
+      fit: BoxFit.contain,
+    );
+  }
+}
