c6bd8fc87fb9ef82c421eae1455bb8e6cf12ec25
somavoyager <66895802+somavoyager@users.noreply.github.com>
2021-02-03 11:36:02 -0500

    Initial host messaging
    Co-authored-by: virenvvarasadiya <virenvarasadiya20@gmail.com>

---
+++ lib/screens/club_screen/club_action_buttons_view/club_message/list_of_club_member_bottomsheet.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:pokerapp/models/club_members_model.dart';
+import 'package:pokerapp/resources/app_colors.dart';
+import 'package:pokerapp/resources/app_styles.dart';
+import 'package:pokerapp/services/app/club_interior_service.dart';
+
+import 'club_host_messaging.dart';
+
+class ListOfClubMemberBottomSheet extends StatelessWidget {
+  final String clubCode;
+  const ListOfClubMemberBottomSheet({
+    Key key,
+    @required this.clubCode,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.screenBackgroundColor,
+      height: 3 * MediaQuery.of(context).size.height / 4,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              "Choose a Member",
+              style: AppStyles.optionTitleText,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<List<ClubMemberModel>>(
+                future: ClubInteriorService.getMembers(clubCode),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  print(snapshot.data);
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ClubHostMessaging(
+                                  clubCode: clubCode,
+                                  player: snapshot.data[index].playerId,
+                                  name: snapshot.data[index].name,
+                                ),
+                              ));
+                        },
+                        child: Container(
+                          padding:
+                              EdgeInsets.only(bottom: 5, top: 10, left: 10),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8),
+                                    child: CircleAvatar(
+                                      radius: 25,
+                                      child: Text(
+                                        snapshot.data[index].name[0]
+                                            .toUpperCase(),
+                                        style: AppStyles.titleTextStyle,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    snapshot.data[index].name,
+                                    style: AppStyles.notificationTitleTextStyle,
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Divider(
+                                color: AppColors.contentColor,
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
