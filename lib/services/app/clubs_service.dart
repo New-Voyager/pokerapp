0edf9851e1187b249c5e525c62167ce3fc48cfca
virenvvarasadiya <virenvarasadiya20@gmail.com>
2021-02-03 20:40:52 +0530

    club-chat-functionality-added

--- lib/services/app/clubs_service.dart
+++ lib/services/app/clubs_service.dart
@@ -6,12 +6,202 @@ import 'package:pokerapp/main.dart';
 import 'package:pokerapp/models/club_homepage_model.dart';
 import 'package:pokerapp/models/club_model.dart';
 import 'package:pokerapp/models/club_weekly_activity_model.dart';
+import 'package:pokerapp/models/host_message_summary_model.dart';
+import 'package:pokerapp/models/messages_from_member.dart';
 import 'package:pokerapp/services/graphQL/mutations/clubs.dart';
 import 'package:pokerapp/services/graphQL/queries/clubs.dart';
 
 import 'package:flutter/services.dart' show rootBundle;
 
 class ClubsService {
+  static String sendPlayerMessageQuery = """
+  mutation sendPlayer(\$clubCode: String!, \$player: String!, \$text: String!) {
+    sendMessageToMember(clubCode: \$clubCode, playerId:\$player, text: \$text) {
+      memberID
+      messageTime
+      messageType
+    }
+  }
+  """;
+
+  static String playerToHostQuery = """
+    mutation playerToHost(\$clubCode: String!, \$text: String!){
+    sendMessageToHost(clubCode:\$clubCode, text: \$text) {
+        messageTime
+        messageType
+      }
+  }
+  """;
+
+  static String memberMessagesQuery = """
+  query memberMessages(\$clubCode:String!, \$player: String!) {
+    messagesFromMember(clubCode:\$clubCode, playerId: \$player) {
+      id
+      memberID
+      messageType
+      text
+      messageTime
+    }
+  }
+  """;
+
+  static String hostmessagesQuery = """
+  query hostmessages(\$clubCode:String!) {
+    messagesFromHost(clubCode:\$clubCode) {
+      id
+      memberID
+      messageType
+      text
+      messageTime
+    }
+  }
+  """;
+
+  static String hostMessageSummaryQuery = """
+  query hostMessageSummary(\$clubCode: String!) {
+    hostMessageSummary (clubCode: \$clubCode){
+      memberID
+      memberName
+      lastMessageText
+      lastMessageTime
+      newMessageCount
+    }
+  }
+  """;
+
+  static String markMemberReadQuery = """
+    mutation markMemberRead(\$clubCode: String!, \$player: String!) {
+      markMemberMsgRead(clubCode: \$clubCode, playerId: \$player)
+    }
+  """;
+
+  static String markreadHostQuery = """
+  mutation markread(\$clubCode: String!) {
+    markHostMsgRead(clubCode: \$clubCode)
+  }
+  """;
+
+  static Future<bool> markMemberRead({String player, String clubCode}) async {
+    GraphQLClient _client = graphQLConfiguration.clientToQuery();
+    Map<String, dynamic> variables;
+    QueryResult result;
+    if (player != null) {
+      variables = {
+        "clubCode": clubCode,
+        "player": player,
+      };
+      result = await _client.mutate(
+        MutationOptions(
+          documentNode: gql(markMemberReadQuery),
+          variables: variables,
+        ),
+      );
+    } else {
+      variables = {
+        "clubCode": clubCode,
+      };
+      result = await _client.mutate(
+        MutationOptions(
+          documentNode: gql(markreadHostQuery),
+          variables: variables,
+        ),
+      );
+    }
+
+    if (result.hasException) return false;
+
+    return player != null
+        ? result.data['markMemberMsgRead'] ?? false
+        : result.data['markHostMsgRead'] ?? false;
+  }
+
+  static Future<List<HostMessageSummaryModel>> hostMessageSummary(
+      {String clubCode}) async {
+    GraphQLClient _client = graphQLConfiguration.clientToQuery();
+    List<HostMessageSummaryModel> allMemberMessages =
+        List<HostMessageSummaryModel>();
+    Map<String, dynamic> variables = {"clubCode": clubCode};
+
+    QueryResult result;
+    result = await _client.query(QueryOptions(
+        documentNode: gql(hostMessageSummaryQuery), variables: variables));
+
+    print("result.data ${result.data} ${result.hasException}");
+    if (result.hasException) return [];
+    result.data['hostMessageSummary'].forEach((e) {
+      print("playerId ${e['playerId']}");
+      allMemberMessages.add(HostMessageSummaryModel.fromJson(e));
+    });
+
+    return allMemberMessages;
+  }
+
+  static Future<List<MessagesFromMember>> memberMessages(
+      {String clubCode, String player}) async {
+    GraphQLClient _client = graphQLConfiguration.clientToQuery();
+    List<MessagesFromMember> messages = List<MessagesFromMember>();
+    print("clubCode $clubCode player $player");
+    if (player != null) {
+      Map<String, dynamic> variables = {"clubCode": clubCode, "player": player};
+      QueryResult result;
+      result = await _client.query(QueryOptions(
+          documentNode: gql(memberMessagesQuery), variables: variables));
+
+      print("result.data ${result.data} ${result.hasException}");
+      if (result.hasException) return [];
+      result.data['messagesFromMember'].forEach((e) {
+        messages.add(MessagesFromMember.fromJson(e));
+      });
+    } else {
+      Map<String, dynamic> variables = {"clubCode": clubCode};
+      QueryResult result;
+      result = await _client.query(QueryOptions(
+          documentNode: gql(hostmessagesQuery), variables: variables));
+      print("result.data ${result.data} ${result.hasException}");
+      if (result.hasException) return [];
+      result.data['messagesFromHost'].forEach((e) {
+        messages.add(MessagesFromMember.fromJson(e));
+      });
+    }
+
+    return messages;
+  }
+
+  static Future sendMessage(String text, String player, String clubCode) async {
+    GraphQLClient _client = graphQLConfiguration.clientToQuery();
+    Map<String, dynamic> variables;
+    if (player != null) {
+      variables = {
+        "clubCode": clubCode,
+        "player": player,
+        "text": text,
+      };
+    } else {
+      variables = {
+        "clubCode": clubCode,
+        "text": text,
+      };
+    }
+
+    QueryResult result;
+    if (player != null) {
+      result = await _client.mutate(
+        MutationOptions(
+          documentNode: gql(sendPlayerMessageQuery),
+          variables: variables,
+        ),
+      );
+    } else {
+      result = await _client.mutate(
+        MutationOptions(
+          documentNode: gql(playerToHostQuery),
+          variables: variables,
+        ),
+      );
+    }
+    if (result.hasException) return false;
+  }
+
   static Future<bool> deleteClub(String clubCode) async {
     GraphQLClient _client = graphQLConfiguration.clientToQuery();
     String _query = Club.deleteClub(clubCode);
