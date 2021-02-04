70984cddfd30f6fd63aa40584efdc9c1fc935183
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2021-02-05 01:32:36 +0530

    added card flip - BUGS introduced

--- lib/screens/game_play_screen/card_views/visible_card_view.dart
+++ lib/screens/game_play_screen/card_views/visible_card_view.dart
@@ -1,8 +1,12 @@
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:pokerapp/resources/app_dimensions.dart';
+import 'package:pokerapp/resources/card_back_assets.dart';
 import 'package:pokerapp/widgets/card_view.dart';
 import 'package:provider/provider.dart';
+
+// TODO: REFACTOR THIS VIEW
 
 class VisibleCardView extends StatelessWidget {
   final GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
@@ -24,21 +28,31 @@ class VisibleCardView extends StatelessWidget {
     Widget cardWidget = cardView.buildCardWidget(context);
     /* for visible cards, the smaller card size is shown to the left of user,
     * and the bigger size is shown as the community card */
+
+    String cardBackAsset = CardBackAssets.asset1_1;
+
+    try {
+      cardBackAsset =
+          Provider.of<ValueNotifier<String>>(context, listen: false).value;
+    } catch (_) {}
+
     return Transform.scale(
       scale: isNotCommunityCard ? 0.85 : 1.4,
       child: Container(
         height: cardView.height,
         width: cardView.width,
-        child: cardView.card.isShownAtTable
+        child: true
             ? ClipRRect(
                 borderRadius: BorderRadius.circular(
                   3.0,
                 ),
                 child: FlipCard(
                   key: cardKey,
-                  front: Consumer<ValueNotifier<String>>(
-                    builder: (_, valueNotifier, __) => Image.asset(
-                      valueNotifier.value,
+                  front: SizedBox(
+                    height: AppDimensions.cardHeight * 1.3,
+                    width: AppDimensions.cardWidth * 1.3,
+                    child: Image.asset(
+                      cardBackAsset,
                       fit: BoxFit.fitHeight,
                     ),
                   ),
