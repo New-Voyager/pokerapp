21b43fbb88d887816acd35cd5329218f44393b6b
somavoyager <somavoyager@gmail.com>
2021-03-06 23:03:37 +0530

    Resolved few issues with hostmessaging APIs

--- lib/models/messages_from_member.dart
+++ lib/models/messages_from_member.dart
@@ -4,15 +4,19 @@ class MessagesFromMember {
   String messageType;
   String text;
   String messageTime;
+  String memberName;
+  String playerId;
 
   MessagesFromMember(
       {this.id, this.memberID, this.messageType, this.text, this.messageTime});
 
   MessagesFromMember.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    memberID = json['memberID'];
+    memberID = json['memberId'];
     messageType = json['messageType'];
     text = json['text'];
     messageTime = json['messageTime'];
+    memberName = json['memberName'];
+    playerId = json['playerId'];
   }
 }
