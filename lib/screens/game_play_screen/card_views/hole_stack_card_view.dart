1662cf80c108cb085e067b6241c1926661a1cf42
Vicky <tovickyoff@gmail.com>
2021-03-18 16:13:34 +0530

    - Card Tap Issue Resolved

--- lib/screens/game_play_screen/card_views/hole_stack_card_view.dart
+++ lib/screens/game_play_screen/card_views/hole_stack_card_view.dart
@@ -6,64 +6,48 @@ import 'package:pokerapp/widgets/card_view.dart';
 const double pullUpOffset = -15.0;
 const kDisplacementConstant = 15.0;
 
-class HoleStackCardView extends StatefulWidget {
+class HoleStackCardView extends StatelessWidget {
+
   final List<CardObject> cards;
   final bool deactivated;
   final bool horizontal;
+  final bool isCardVisible;
 
   HoleStackCardView({
     @required this.cards,
     this.deactivated = false,
-    this.horizontal = true,
+    this.horizontal = true, this.isCardVisible = false,
   });
 
-  @override
-  _HoleStackCardViewState createState() => _HoleStackCardViewState();
-}
-
-class _HoleStackCardViewState extends State<HoleStackCardView> {
-  bool isCardVisible;
-
-  _HoleStackCardViewState({this.isCardVisible = false});
 
   @override
   Widget build(BuildContext context) {
-    if (widget.cards == null || widget.cards.isEmpty) return const SizedBox.shrink();
-    double mid = (widget.cards.length / 2);
+    if (cards == null || cards.isEmpty) return const SizedBox.shrink();
+    double mid = (cards.length / 2);
 
-    return GestureDetector(
-      onLongPress: (){
-        setState(()=> isCardVisible = true);
-      },
-      onLongPressEnd: (_){
-        setState(()=> isCardVisible = false);
-      },
-      onTap: () => setState(()=> isCardVisible = !isCardVisible),
-      child: AnimatedContainer(
-        duration: Duration(milliseconds:800 ),
-        child: Transform.translate(
-          offset: Offset(-15,30),
-          child: Stack(
-            alignment: Alignment.bottomLeft,
-            children: List.generate(
-              widget.cards.length,
-                  (i) => Transform.translate(
-                offset: Offset(
-                  kDisplacementConstant * i,
-                  -i * 1.50,
-                ),
-                child: Transform.rotate(
-                  alignment: Alignment.bottomLeft,
-                  angle: (i - mid) * 0.20,
-                  child:Transform.translate(
-                    offset: Offset(
-                      0.0,
-                      widget.cards[i].highlight ? pullUpOffset : 0.0,
-                    ),
-                    child: widget.deactivated
-                        ? GameCardWidget(card: widget.cards[i], grayOut: true,isCardVisible: isCardVisible,)
-                        : GameCardWidget(card: widget.cards[i],isCardVisible: isCardVisible),
+    return Container(
+      child: Transform.translate(
+        offset: Offset(-15,30),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: List.generate(
+            cards.length,
+                (i) => Transform.translate(
+              offset: Offset(
+                kDisplacementConstant * i,
+                -i * 1.50,
+              ),
+              child: Transform.rotate(
+                alignment: Alignment.bottomLeft,
+                angle: (i - mid) * 0.20,
+                child:Transform.translate(
+                  offset: Offset(
+                    0.0,
+                    cards[i].highlight ? pullUpOffset : 0.0,
                   ),
+                  child: deactivated
+                      ? GameCardWidget(card: cards[i], grayOut: true,isCardVisible: isCardVisible)
+                      : GameCardWidget(card: cards[i],isCardVisible: isCardVisible,),
                 ),
               ),
             ),
@@ -72,27 +56,5 @@ class _HoleStackCardViewState extends State<HoleStackCardView> {
       ),
     );
 
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: widget.cards.isEmpty
-          ? [SizedBox.shrink()]
-          : widget.cards.reversed
-              .toList()
-              .map(
-                (c) => Transform.translate(
-                  offset: Offset(
-                    0.0,
-                    c.highlight ? pullUpOffset : 0.0,
-                  ),
-                  child: widget.deactivated
-                      ? CardView(card: c, grayOut: true)
-                      : CardView(card: c),
-                ),
-              )
-              .toList()
-              .reversed
-              .toList(),
-    );
   }
 }
