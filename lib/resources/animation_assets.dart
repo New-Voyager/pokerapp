1943aacffc675560fc07a0924394d0198397345a
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2021-04-14 21:37:47 +0530

    added and cleaned svgs to have inline style

---
+++ lib/resources/animation_assets.dart
@@ -0,0 +1,45 @@
+class AnimationObject {
+  final String id;
+  final String assetSvg;
+  final String assetJson;
+
+  const AnimationObject({
+    this.id,
+    this.assetSvg,
+    this.assetJson,
+  });
+}
+
+class AnimationAssets {
+  AnimationAssets._();
+
+  static const String _base = 'assets/animations';
+
+  static const List<AnimationObject> animationObjects = const [
+    const AnimationObject(
+      id: 'chicken',
+      assetSvg: '$_base/chicken.svg',
+      assetJson: '$_base/chicken.json',
+    ),
+    const AnimationObject(
+      id: 'donkey',
+      assetSvg: '$_base/donkey.svg',
+      assetJson: '$_base/donkey.json',
+    ),
+    const AnimationObject(
+      id: 'fish',
+      assetSvg: '$_base/fish.svg',
+      assetJson: '$_base/fish.json',
+    ),
+    const AnimationObject(
+      id: 'poop',
+      assetSvg: '$_base/poop.svg',
+      assetJson: '$_base/poop.json',
+    ),
+    const AnimationObject(
+      id: 'rat',
+      assetSvg: '$_base/rat.svg',
+      assetJson: '$_base/rat.json',
+    ),
+  ];
+}
