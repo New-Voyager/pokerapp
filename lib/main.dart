65f09d79c502cc72e467860da45352d63b23b921
PradeepReddy <pradeep.reddy@eka.care>
2021-04-21 16:37:31 +0530

    Added firebase services to pubspec.yaml and initialized firebase in main.dart. Added Push notifications capability in xcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:pokerapp/routes.dart';
@@ -19,18 +20,38 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Poker App',
-      debugShowCheckedModeBanner: false,
-      navigatorKey: navigatorKey,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      onGenerateRoute: Routes.generateRoute,
-      initialRoute: Routes.initial,
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          print('Firebase initialization failed! ${snapshot.error.toString()}');
+          return Text('Something went wrong!');
+        }
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          print('Firebase initialized successfully');
+          return MaterialApp(
+            title: 'Poker App',
+            debugShowCheckedModeBanner: false,
+            navigatorKey: navigatorKey,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            onGenerateRoute: Routes.generateRoute,
+            initialRoute: Routes.initial,
+          );
+        }
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
