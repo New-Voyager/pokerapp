4a97df9c670cf49caefb0ac91b45d87af60b50ba
somavoyager <somavoyager@gmail.com>
2021-04-23 20:58:59 +0530

    Added pending approvals model

--- lib/services/firebase/push_notification_service.dart
+++ lib/services/firebase/push_notification_service.dart
@@ -24,13 +24,19 @@ Future<void> saveFirebaseToken(String token) async {
   }
 }
 
-void registerPushNotifications() {
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+void registerPushNotifications()  {
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
     print('Got a message whilst in the foreground!');
     print('Message data: ${message.data}');
 
     if (message.notification != null) {
       print('Message also contained a notification: ${message.notification}');
+    }
+
+    if (message.data['type'].toString() == 'BUYIN_REQUEST') {
+      final approvals = await PlayerService.getPendingApprovals();
+      final approvalCount = await PlayerService.getPendingApprovalsCount();
+      print('approval count: $approvalCount length: ${approvals.length}');
     }
   });
   log('Registered for push notifications');
