0ed0568df84d330e861ceb51780eb15bdc851d12
somavoyager <somavoyager@gmail.com>
2021-05-04 16:29:08 +0530

    Made minor changes for inapp testing

--- lib/screens/main_screens/purchase_page_view/purchase_page_view.dart
+++ lib/screens/main_screens/purchase_page_view/purchase_page_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:pokerapp/resources/app_colors.dart';
+import 'package:pokerapp/services/test/test_service.dart';
 
 class PurchasePageView extends StatefulWidget {
   @override
@@ -8,6 +11,45 @@ class PurchasePageView extends StatefulWidget {
 class _PurchasePageViewState extends State<PurchasePageView> {
   @override
   Widget build(BuildContext context) {
-    return Text("Purchase Page View", style:TextStyle(color: Colors.white),);
+    //return Text("Purchase Page View", style:TextStyle(color: Colors.white),);
+
+    return Scaffold(
+          /* FIXME: THIS FLOATING ACTION BUTTON IS FOR SHOWING THE TESTS */
+          floatingActionButton: floatingActionButton(
+            onReload: () {},
+          ),
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.black,
+          body: Builder(
+            builder: (_) {
+              return Text("Purchase Page View", style:TextStyle(color: Colors.white),);
+            },
+          ),
+        );
   }
+
+ /* THIS SPEED DIAL IS JUST FOR SHOWING THE TEST BUTTONS */
+  static SpeedDial floatingActionButton({
+    Function onReload,
+  }) {
+    return SpeedDial(
+      onOpen: onReload,
+      overlayColor: Colors.black,
+      visible: TestService.isTesting,
+      overlayOpacity: 0.1,
+      icon: Icons.all_inclusive_rounded,
+      children: [
+        SpeedDialChild(
+          child: Icon(
+            Icons.adb_rounded,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.red,
+          label: 'Inapp Products',
+          onTap: () => TestService.testIap(),
+        ),
+      ],
+      backgroundColor: AppColors.appAccentColor,
+    );
+  }  
 }
