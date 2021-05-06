d10628461411d37579e00fa7e473d3d7085fbbfe
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2021-05-06 13:38:19 +0530

    fixed bet chip widget UI

--- lib/screens/game_play_screen/widgets/bet_widget.dart
+++ lib/screens/game_play_screen/widgets/bet_widget.dart
@@ -33,132 +33,145 @@ class BetWidget extends StatelessWidget {
           listen: false,
         );
 
-        return FittedBox(
-          fit: BoxFit.fitHeight,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Transform.translate(
-                offset: Offset(0, 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    /* - button */
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      child: IconButton(
-                        icon: Icon(Icons.remove_circle_rounded),
-                        color: Colors.blue,
-                        onPressed: () {
-                          if (valueNotifierVal.value <= action.minRaiseAmount)
-                            return;
-                          valueNotifierVal.value--;
-                        },
+        return Stack(
+          children: [
+            /* top */
+            Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                children: [
+                  /* (-) button */
+                  Expanded(
+                    flex: 2,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.remove_circle_rounded),
+                          color: Colors.blue,
+                          onPressed: () {
+                            if (valueNotifierVal.value <= action.minRaiseAmount)
+                              return;
+                            valueNotifierVal.value--;
+                          },
+                        ),
                       ),
                     ),
+                  ),
 
-                    /* center widget */
-                    Container(
-                      child: Stack(
-                        children: [
-                          /* seek slider */
-                          Align(
-                            alignment: Alignment.center,
-                            child: Container(
-                              margin: EdgeInsets.only(bottom: 16),
-                              height: height / 5,
-                              width: width / 2,
-                              child: sleekSlider(),
-                            ),
+                  /* center widget - bet amount, bet button & slider */
+                  Expanded(
+                    flex: 3,
+                    child: Stack(
+                      children: [
+                        /* seek slider */
+                        Align(
+                          alignment: Alignment.center,
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 16),
+                            height: height / 5,
+                            width: width / 2,
+                            child: sleekSlider(),
                           ),
+                        ),
 
-                          /* bet button */
-                          Positioned(
-                            top: 0,
-                            bottom: 0,
-                            right: 0,
-                            left: 0,
-                            child: Transform.translate(
-                              offset: Offset(0, 40),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Consumer<ValueNotifier<double>>(
-                                    builder: (_, vnVal, __) => Container(
-                                      width: 70,
-                                      height: 70,
-                                      child: PulsatingCircleIconButton(
-                                        onTap: () =>
-                                            onSubmitCallBack?.call(vnVal.value),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                              DataFormatter.chipsFormat(
-                                                vnVal.value,
-                                              ),
-                                              style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.white,
-                                              ),
+                        /* bet button */
+                        Align(
+                          alignment: Alignment.center,
+                          child: Transform.translate(
+                            offset: const Offset(0.0, -20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Consumer<ValueNotifier<double>>(
+                                  builder: (_, vnVal, __) => Container(
+                                    width: 70,
+                                    height: 70,
+                                    child: PulsatingCircleIconButton(
+                                      onTap: () =>
+                                          onSubmitCallBack?.call(vnVal.value),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            DataFormatter.chipsFormat(
+                                              vnVal.value,
                                             ),
-                                            SizedBox(height: 5),
-                                            Text(
-                                              "BET",
-                                              style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.white,
-                                              ),
-                                            )
-                                          ],
-                                        ),
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            "BET",
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.white,
+                                            ),
+                                          )
+                                        ],
                                       ),
                                     ),
-                                  )
-                                ],
-                              ),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-
-                    /* + button */
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.add_circle_rounded,
                         ),
-                        color: Colors.blue,
-                        onPressed: () {
-                          if (valueNotifierVal.value >= action.maxRaiseAmount)
-                            return;
-                          valueNotifierVal.value++;
-                        },
+                      ],
+                    ),
+                  ),
+
+                  /* (+) button */
+                  Expanded(
+                    flex: 2,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.add_circle_rounded,
+                          ),
+                          color: Colors.blue,
+                          onPressed: () {
+                            if (valueNotifierVal.value >= action.maxRaiseAmount)
+                              return;
+                            valueNotifierVal.value++;
+                          },
+                        ),
                       ),
                     ),
-                  ],
+                  ),
+                ],
+              ),
+            ),
+
+            /* button row for other bet options */
+            Align(
+              alignment: Alignment.center,
+              child: Transform.translate(
+                offset: const Offset(0.0, 64.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: width / 1.5,
+                  height: 64,
+                  child: betAmountList(valueNotifierVal),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 4),
-                alignment: Alignment.center,
-                width: width / 1.5,
-                height: 64,
-                child: betAmountList(valueNotifierVal),
-              )
-            ],
-          ),
+            )
+          ],
         );
       },
     );
@@ -205,70 +218,28 @@ class BetWidget extends StatelessWidget {
     );
   }
 
-  Widget betAmountList(ValueNotifier<double> vnValue) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          // show keyboard
-          return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(children: [
+  Widget _buildBetAmountChild({
+    bool isKeyboard = false,
+    Option option,
+    void onTap(),
+  }) =>
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               InkWell(
-                onTap: () async {
-                  double min = action.minRaiseAmount.toDouble();
-                  double max = action.maxRaiseAmount.toDouble();
-
-                  final double res = await NumericKeyboard.show(
-                    context,
-                    title: 'Enter your bet/raise amount ($min - $max)',
-                    min: min,
-                    max: max,
-                  );
-
-                  if (res != null) vnValue.value = res;
-                },
+                onTap: onTap,
                 child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 4, 0, 0),
-                  padding: EdgeInsets.all(4),
+                  padding: isKeyboard
+                      ? const EdgeInsets.all(4)
+                      : const EdgeInsets.all(8),
+                  margin: isKeyboard
+                      ? const EdgeInsets.fromLTRB(0, 4, 0, 0)
+                      : const EdgeInsets.symmetric(vertical: 4),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 1.0),
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      gradient: LinearGradient(
-                        // begin: Alignment.topRight,
-                        // end: Alignment.bottomLeft,
-                        colors: [
-                          Colors.indigo,
-                          Colors.cyan,
-                        ],
-                      )),
-                  child: Icon(Icons.keyboard, color: Colors.white),
-                ),
-              ),
-              Text(
-                '',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.white,
-                ),
-              ),
-            ]),
-            SizedBox(width: 10),
-          ]);
-        }
-
-        final option = action.options[index - 1];
-        return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Column(children: [
-            InkWell(
-              onTap: () {
-                vnValue.value = option.amount.toDouble();
-              },
-              child: Container(
-                padding: EdgeInsets.all(8),
-                margin: EdgeInsets.symmetric(vertical: 4),
-                decoration: BoxDecoration(
                     //color: Colors.red,
                     border: Border.all(color: Colors.white, width: 1.0),
                     shape: BoxShape.circle,
@@ -279,23 +250,68 @@ class BetWidget extends StatelessWidget {
                         Colors.indigo,
                         Colors.cyan,
                       ],
-                    )),
-                child: Text(
-                  "${option.text}",
-                  style: TextStyle(fontSize: 10, color: Colors.white),
+                    ),
+                  ),
+                  child: isKeyboard
+                      ? Icon(
+                          Icons.keyboard,
+                          color: Colors.white,
+                        )
+                      : Text(
+                          "${option.text}",
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                          ),
+                        ),
                 ),
               ),
-            ),
-            Text(
-              "${option.amount.toInt().toString()}",
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.white,
+              Text(
+                isKeyboard ? '' : '${option.amount.toInt().toString()}',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ]),
+            ],
+          ),
           SizedBox(width: 10),
-        ]);
+        ],
+      );
+
+  Widget betAmountList(ValueNotifier<double> vnValue) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          // show keyboard
+          return _buildBetAmountChild(
+            isKeyboard: true,
+            onTap: () async {
+              double min = action.minRaiseAmount.toDouble();
+              double max = action.maxRaiseAmount.toDouble();
+
+              final double res = await NumericKeyboard.show(
+                context,
+                title: 'Enter your bet/raise amount ($min - $max)',
+                min: min,
+                max: max,
+              );
+
+              if (res != null) vnValue.value = res;
+            },
+          );
+        }
+
+        final option = action.options[index - 1];
+
+        return _buildBetAmountChild(
+          option: action.options[index - 1],
+          onTap: () {
+            vnValue.value = option.amount.toDouble();
+          },
+        );
       },
       itemCount: action.options.length + 1,
     );
