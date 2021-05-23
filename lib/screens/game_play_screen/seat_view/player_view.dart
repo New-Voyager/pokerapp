23b40a8fb9069ae9afc4f04fc7a46cb96376e336
somavoyager <somavoyager@gmail.com>
2021-05-23 20:40:37 +0530

    Changed color of mic/speaker color

--- lib/screens/game_play_screen/seat_view/player_view.dart
+++ lib/screens/game_play_screen/seat_view/player_view.dart
@@ -237,36 +237,39 @@ class PlayerView extends StatelessWidget {
                   visible: seat.player.talking,
                   child: Positioned(
                       top: 0,
-                      right: -12,
+                      right: -20,
                       child: Container(
                           width: 22,
                           height: 22,
-                          color: Colors.white,
+                          color: Colors.transparent,
                           child: Icon(
                             Icons.volume_up_outlined,
+                            color: Colors.white70,
                           )))),
               seat.player.showMicOff
                   ? Positioned(
                       top: 0,
-                      right: -12,
+                      right: -20,
                       child: Container(
                           width: 22,
                           height: 22,
-                          color: Colors.white,
+                          color: Colors.transparent,
                           child: Icon(
                             Icons.mic_off,
+                            color: Colors.white70,
                           )))
                   : SizedBox(),
               seat.player.showMicOn
                   ? Positioned(
                       top: 0,
-                      right: -12,
+                      right: -20,
                       child: Container(
                           width: 22,
                           height: 22,
-                          color: Colors.white,
+                          color: Colors.transparent,
                           child: Icon(
                             Icons.mic,
+                            color: Colors.white70,
                           )))
                   : SizedBox(),
             ],
