306545284ae4382428c2e323d99a45571df81995
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2021-05-31 08:58:01 +0530

    minor change to main.dart making fields final in an immutable class

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  Nats nats;
+  final Nats nats = Nats();
   // Create the initialization Future outside of `build`:
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    this.nats = Nats();
+    // this.nats = Nats();
     return FutureBuilder(
       // Initialize FlutterFire:
       future: _initialization,
