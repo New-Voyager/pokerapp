72686f6a4b3b493d6d1d0c4f960e1e21cda8a629
Srinivas Reddy G <sreenu240@gmail.com>
2021-06-02 09:48:50 +0530

    Buttons created,dialog for game selection
    choose game screen UI created with animation, dialog box
    
    newbutton widget created

---
+++ lib/screens/game_screens/widgets/new_button_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pokerapp/resources/new/app_colors_new.dart';
+
+class NewButton extends StatelessWidget {
+  final String text;
+  final Function onTapFunc;
+  final ButtonStyle style;
+  NewButton({this.text, this.onTapFunc, this.style});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTapFunc,
+      style: style,
+      child: Container(
+        child: Text(text),
+      ),
+    );
+  }
+}
