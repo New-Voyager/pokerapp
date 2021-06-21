cdc9302a6e1b97c363e1d36a80e5392872ef0983
Srinivas Reddy G <sreenu240@gmail.com>
2021-06-21 22:13:06 +0530

    inital commit for clubstats

--- lib/services/app/stats_service.dart
+++ lib/services/app/stats_service.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:pokerapp/main.dart';
+import 'package:pokerapp/models/club_stats_model.dart';
 import 'package:pokerapp/models/hand_stats_model.dart';
 
 class StatsService {
@@ -57,28 +58,123 @@ query (\$gameCode: String!) {
 }
   """;
 
+  static final String clubStatsQuery = """
+
+query clubStats(\$clubCode: String!) {
+  holdem: clubStats(gameType: HOLDEM, clubCode: \$clubCode) {
+    straightAFlush
+    straightKFlush
+    straightQFlush
+    straightJFlush
+    straightTFlush
+    straight9Flush
+    straight8Flush
+    straight7Flush
+    straight6Flush
+    straight5Flush
+
+    fourAAAA
+    fourKKKK
+    fourQQQQ
+    fourJJJJ
+    fourTTTT
+    four9999
+    four8888
+    four7777
+    four6666
+    four5555
+    four4444
+    four3333
+    four2222
+
+    totalHands
+  }
+  plo: clubStats(gameType: PLO, clubCode: \$clubCode) {
+    straightAFlush
+    straightKFlush
+    straightQFlush
+    straightJFlush
+    straightTFlush
+    straight9Flush
+    straight8Flush
+    straight7Flush
+    straight6Flush
+    straight5Flush
+
+    fourAAAA
+    fourKKKK
+    fourQQQQ
+    fourJJJJ
+    fourTTTT
+    four9999
+    four8888
+    four7777
+    four6666
+    four5555
+    four4444
+    four3333
+    four2222
+
+    totalHands
+
+  }
+    fivecard_plo: clubStats(gameType: FIVE_CARD_PLO, clubCode: \$clubCode) {
+    straightAFlush
+    straightKFlush
+    straightQFlush
+    straightJFlush
+    straightTFlush
+    straight9Flush
+    straight8Flush
+    straight7Flush
+    straight6Flush
+    straight5Flush
+
+    fourAAAA
+    fourKKKK
+    fourQQQQ
+    fourJJJJ
+    fourTTTT
+    four9999
+    four8888
+    four7777
+    four6666
+    four5555
+    four4444
+    four3333
+    four2222
+
+    totalHands
+  }
+}
+
+  """;
+
   static Future<HandStatsModel> getStatsForAGame(String gameCode) async {
     GraphQLClient _client = graphQLConfiguration.clientToQuery();
     Map<String, dynamic> variables = {
       "gameCode": gameCode,
     };
-    String query = getStatsForGameQuery;
-    QueryResult result = await _client
-        .query(QueryOptions(documentNode: gql(query), variables: variables));
+    QueryResult result = await _client.query(QueryOptions(
+        documentNode: gql(getStatsForGameQuery), variables: variables));
 
     if (result.hasException) return null;
 
-    // instantiate game history detail object
-    // model.jsonData = result.data;
-    // model.load();
-    //final d = jsonEncode(result.data);
-    // HandlogCacheService.saveToCache(gameCode, handNum, data, prefs);
-
-    // log("-=-=-STAT raw DATA\n: ${result.data.data}");
-    // log(
-    //   "-=-=-STAT DATA\n $d",
-    // );
     final handStats = HandStatsModel.fromJson(result.data.data);
     return handStats;
+  }
+
+  static Future<ClubStatsModel> getClubStats(String clubCode) async {
+    GraphQLClient _client = graphQLConfiguration.clientToQuery();
+    Map<String, dynamic> variables = {
+      "clubCode": clubCode,
+    };
+    QueryResult result = await _client.query(
+        QueryOptions(documentNode: gql(clubStatsQuery), variables: variables));
+
+    if (result.hasException) return null;
+
+    final clubStats = ClubStatsModel.fromJson(result.data.data);
+    return clubStats;
   }
 }
