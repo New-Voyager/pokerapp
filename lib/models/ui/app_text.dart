826fe3078235389c210a3b134497daf28dc84a0c
somavoyager <66895802+somavoyager@users.noreply.github.com>
2021-08-06 22:54:26 -0400

    initial work for internationalization (#392)
    Co-authored-by: somavoyager <somavoyager@gmail.com>

---
+++ lib/models/ui/app_text.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class AppText {
+  Map<String, Map<String, String>> languageTexts =
+      Map<String, Map<String, String>>();
+
+  AppText();
+
+  getText(String screenName, String textId) {
+    if (languageTexts.containsKey(screenName)) {
+      if (languageTexts[screenName].containsKey(textId)) {
+        return languageTexts[screenName][textId];
+      }
+    }
+    return "No Text";
+  }
+
+  AppTextScreen getScreen(String screenName) {
+    if (languageTexts.containsKey(screenName)) {
+      return AppTextScreen(languageTexts[screenName]);
+    }
+    return AppTextScreen(Map<String, String>());
+  }
+
+  void change(String lang) async {
+    String json;
+    try {
+      json = await rootBundle.loadString('assets/language/$lang.json');
+    } catch (err) {
+      json = await rootBundle.loadString('assets/language/en.json');
+    }
+    if (json != null) {
+      languageTexts = Map<String, Map<String, String>>();
+      Map<String, dynamic> screens = jsonDecode(json);
+      for (String key in screens.keys) {
+        dynamic screenTexts = screens[key];
+        languageTexts[key] = Map<String, String>();
+        for (String textId in screenTexts.keys) {
+          languageTexts[key][textId] = screenTexts[textId].toString();
+        }
+      }
+    }
+  }
+
+  static Future<AppText> init(String lang) async {
+    final text = AppText();
+    text.change(lang);
+    return text;
+  }
+}
+
+class AppTextScreen {
+  Map<String, String> texts;
+  AppTextScreen(this.texts);
+  String getText(String textId) {
+    if (texts.containsKey(textId)) {
+      return texts[textId];
+    }
+    return "No Text";
+  }
+
+  String operator [](String textId) {
+    return getText(textId);
+  }
+}
+
+AppText appText;
+
+initAppText(String lang) async {
+  appText = await AppText.init(lang);
+}
+
+AppText getAppText() {
+  return appText;
+}
+
+AppTextScreen getAppTextScreen(String screenName) {
+  return appText.getScreen(screenName);
+}
