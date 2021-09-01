89e698d07c7234b217258730b2ca85311b244c4e
Jyotirmoy Paul <mr.jyotirmoy.paul@gmail.com>
2021-09-01 10:02:05 +0530

    fixes potview recalculation after seat change of players

--- lib/screens/game_play_screen/seat_view/player_view.dart
+++ lib/screens/game_play_screen/seat_view/player_view.dart
@@ -60,15 +60,36 @@ class PlayerView extends StatefulWidget {
   _PlayerViewState createState() => _PlayerViewState();
 }
 
+class NeedRecalculating {
+  bool value = false;
+}
+
 class _PlayerViewState extends State<PlayerView> with TickerProviderStateMixin {
   TableState _tableState;
+
+  HandInfoState _handInfoState;
+  NeedRecalculating _seatPosNeedsReCalculating = NeedRecalculating();
+  int _lastHandNum = 0;
 
   AnimationController _lottieController;
   AssetImage _gifAssetImage;
 
+  void handInfoStateListener() {
+    if (_handInfoState.handNum != _lastHandNum) {
+      _lastHandNum = _handInfoState.handNum;
+      log('pauldebug: SETTING TRUE');
+      _seatPosNeedsReCalculating.value = true;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+
+    _handInfoState = context.read<HandInfoState>();
+
+    // setup condition to notify when recalculating is necessary
+    _handInfoState.addListener(handInfoStateListener);
 
     _lottieController = AnimationController(
       vsync: this,
@@ -89,6 +110,7 @@ class _PlayerViewState extends State<PlayerView> with TickerProviderStateMixin {
   @override
   void dispose() {
     super.dispose();
+    _handInfoState?.removeListener(handInfoStateListener);
     _lottieController?.dispose();
   }
 
@@ -252,6 +274,7 @@ class _PlayerViewState extends State<PlayerView> with TickerProviderStateMixin {
     bool animate = widget.seat.player.action.animateAction;
 
     Widget chipAmountWidget = ChipAmountWidget(
+      recalculatingNeeded: _seatPosNeedsReCalculating,
       animate: animate,
       potKey: boardAttributes.getPotsKey(0),
       key: widget.seat.betWidgetUIKey,
@@ -333,12 +356,7 @@ class _PlayerViewState extends State<PlayerView> with TickerProviderStateMixin {
 
               // /* building the chip amount widget */
               animate
-                  ? ChipAmountAnimatingWidget(
-                      key: ValueKey(_tableState.tableRefresh),
-                      seatPos: widget.seat.serverSeatPos,
-                      child: chipAmountWidget,
-                      reverse: widget.seat.player.action.winner,
-                    )
+                  ? _animatingChipAmount(chipAmountWidget)
                   : chipAmountWidget,
 
               Consumer<SeatChangeNotifier>(
@@ -408,6 +426,15 @@ class _PlayerViewState extends State<PlayerView> with TickerProviderStateMixin {
           ),
         );
       },
+    );
+  }
+
+  ChipAmountAnimatingWidget _animatingChipAmount(Widget chipAmountWidget) {
+    return ChipAmountAnimatingWidget(
+      key: ValueKey(_tableState.tableRefresh),
+      seatPos: widget.seat.serverSeatPos,
+      child: chipAmountWidget,
+      reverse: widget.seat.player.action.winner,
     );
   }
 
