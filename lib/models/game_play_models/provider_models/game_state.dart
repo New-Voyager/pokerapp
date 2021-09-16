ef123ef5308e1a6dec397221943314cbd911907d
somavoyager <somavoyager@gmail.com>
2021-09-16 10:58:53 -0400

    Moved GameAssets class out of game_state.dart

--- lib/models/game_play_models/provider_models/game_state.dart
+++ lib/models/game_play_models/provider_models/game_state.dart
@@ -21,10 +21,8 @@ import 'package:pokerapp/services/agora/agora.dart';
 import 'package:pokerapp/services/app/asset_service.dart';
 import 'package:pokerapp/services/app/game_service.dart';
 import 'package:pokerapp/services/app/handlog_cache_service.dart';
-import 'package:pokerapp/services/data/asset_hive_store.dart';
 import 'package:pokerapp/services/data/game_hive_store.dart';
 import 'package:pokerapp/services/data/hive_models/game_settings.dart';
-import 'package:pokerapp/services/data/user_settings_store.dart';
 import 'package:pokerapp/services/game_play/game_com_service.dart';
 import 'package:pokerapp/services/game_play/game_messaging_service.dart';
 import 'package:pokerapp/services/janus/janus.dart';
@@ -33,6 +31,7 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:pokerapp/proto/hand.pb.dart' as proto;
 
+import 'game_assets.dart';
 import 'host_seat_change.dart';
 import 'player_action.dart';
 import 'table_state.dart';
@@ -1319,111 +1318,6 @@ class CommunicationState extends ChangeNotifier {
 class StraddlePromptState extends ChangeNotifier {
   void notify() {
     notifyListeners();
-  }
-}
-
-class GameScreenAssets {
-  String backdropImage;
-  String boardImage;
-  String holeCardBackImage;
-  String holeCardFaceDir;
-
-  Uint8List backdropBytes;
-  Uint8List boardBytes;
-  Uint8List holeCardBackBytes;
-  Uint8List betImageBytes;
-  Map<String, Uint8List> cardStrImage;
-  Map<int, Uint8List> cardNumberImage;
-
-  Uint8List getBackDrop() {
-    return backdropBytes;
-  }
-
-  Uint8List getBoard() {
-    return boardBytes;
-  }
-
-  Uint8List getHoleCardBack() {
-    return holeCardBackBytes;
-  }
-
-  Uint8List getHoleCard(int card) {
-    return cardNumberImage[card];
-  }
-
-  Uint8List getHoleCardStr(String card) {
-    return cardStrImage[card];
-  }
-
-  Uint8List getBetImage() {
-    return betImageBytes;
-  }
-
-  Future<void> initialize() async {
-    cardStrImage = Map<String, Uint8List>();
-    cardNumberImage = Map<int, Uint8List>();
-    Asset backdrop =
-        AssetService.getAssetForId(UserSettingsStore.getSelectedBackdropId());
-    if (backdrop == null) {
-      backdrop =
-          AssetService.getAssetForId(UserSettingsStore.VALUE_DEFAULT_BACKDROP);
-    }
-    backdropBytes = await backdrop.getBytes();
-
-    Asset table =
-        AssetService.getAssetForId(UserSettingsStore.getSelectedTableId());
-    if (table == null) {
-      table = AssetService.getAssetForId(UserSettingsStore.VALUE_DEFAULT_TABLE);
-    }
-    boardBytes = await table.getBytes();
-
-    Asset betImage =
-        AssetService.getAssetForId(UserSettingsStore.getSelectedBetDial());
-    if (betImage == null) {
-      betImage =
-          AssetService.getAssetForId(UserSettingsStore.VALUE_DEFAULT_BETDIAL);
-    }
-    betImageBytes = await betImage.getBytes();
-
-    Asset cardBack =
-        AssetService.getAssetForId(UserSettingsStore.getSelectedCardBackId());
-    if (cardBack == null) {
-      cardBack =
-          AssetService.getAssetForId(UserSettingsStore.VALUE_DEFAULT_CARDBACK);
-    }
-    holeCardBackBytes = await cardBack.getBytes();
-
-    Asset cardFace =
-        AssetService.getAssetForId(UserSettingsStore.getSelectedCardFaceId());
-    if (cardFace == null) {
-      cardFace =
-          AssetService.getAssetForId(UserSettingsStore.VALUE_DEFAULT_CARDFACE);
-    }
-
-    for (int card in CardConvUtils.cardNumbers.keys) {
-      final cardStr = CardConvUtils.getString(card);
-      Uint8List cardBytes;
-      if (cardFace.bundled ?? false) {
-        cardBytes =
-            (await rootBundle.load('${cardFace.downloadDir}/$cardStr.svg'))
-                .buffer
-                .asUint8List();
-      } else {
-        String filename = '${cardFace.downloadDir}/$card.svg';
-        if (!File(filename).existsSync()) {
-          filename = '${cardFace.downloadDir}/$card.png';
-          if (!File(filename).existsSync()) {
-            filename = '${cardFace.downloadDir}/$cardStr.svg';
-            if (!File(filename).existsSync()) {
-              filename = '${cardFace.downloadDir}/$cardStr.png';
-            }
-          }
-        }
-        cardBytes = File(filename).readAsBytesSync();
-      }
-      cardStrImage[cardStr] = cardBytes;
-      cardNumberImage[card] = cardBytes;
-    }
   }
 }
 
