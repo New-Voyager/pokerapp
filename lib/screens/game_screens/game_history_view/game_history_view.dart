248aa0236aa1d8386eef8935d4ca4de05f98d08e
Jyotirmoy Paul <jyotirmoy.paul@ril.com>
2021-10-27 21:33:33 +0530

    adds month picker

--- lib/screens/game_screens/game_history_view/game_history_view.dart
+++ lib/screens/game_screens/game_history_view/game_history_view.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_date_pickers/flutter_date_pickers.dart' as fdp;
 import 'package:pokerapp/main.dart';
 import 'package:pokerapp/models/game_history_model.dart';
 import 'package:pokerapp/models/ui/app_text.dart';
@@ -10,6 +13,7 @@ import 'package:pokerapp/screens/chat_screen/widgets/no_message.dart';
 import 'package:pokerapp/screens/game_screens/game_history_view/game_history_item_new.dart';
 import 'package:pokerapp/screens/game_screens/widgets/back_button.dart';
 import 'package:pokerapp/services/app/club_interior_service.dart';
+import 'package:pokerapp/widgets/dialogs.dart';
 import 'package:provider/provider.dart';
 
 class GameHistoryView extends StatefulWidget {
@@ -83,6 +87,74 @@ class _GameHistoryViewState extends State<GameHistoryView>
     );
   }
 
+  Widget _buildMonthDatePicker({
+    @required final DateTime selectedDate,
+    @required final DateTime firstAllowedDate,
+    @required final DateTime lastAllowedDate,
+    @required final ValueChanged<DateTime> onNewSelected,
+    @required final AppTheme theme,
+  }) {
+    // add some colors to default settings
+    fdp.DatePickerStyles styles = fdp.DatePickerStyles(
+      selectedSingleDateDecoration: BoxDecoration(
+        color: theme.accentColor,
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+    );
+
+    final fdp.DatePickerLayoutSettings layoutSettings =
+        fdp.DatePickerLayoutSettings(
+      scrollPhysics: BouncingScrollPhysics(),
+    );
+
+    final selectedDateNotifier = ValueNotifier<DateTime>(selectedDate);
+
+    return ValueListenableBuilder(
+      valueListenable: selectedDateNotifier,
+      builder: (_, selectedDate, __) => fdp.MonthPicker.single(
+        selectedDate: selectedDate,
+        onChanged: (newDate) {
+          selectedDateNotifier.value = newDate;
+          onNewSelected(newDate);
+        },
+        firstDate: firstAllowedDate,
+        lastDate: lastAllowedDate,
+        datePickerLayoutSettings: layoutSettings,
+        datePickerStyles: styles,
+      ),
+    );
+  }
+
+  void _showMonthPickerDialog({
+    @required final AppTheme theme,
+  }) async {
+    DateTime pickedDate;
+
+    final needToFilter = await showPrompt(
+      context,
+      "Filter",
+      "Month Picker",
+      child: _buildMonthDatePicker(
+        theme: theme,
+        selectedDate: DateTime.now(),
+        firstAllowedDate: DateTime.now().subtract(
+          Duration(days: 365),
+        ),
+        lastAllowedDate: DateTime.now(),
+        onNewSelected: (DateTime dateTime) {
+          pickedDate = dateTime;
+          // Navigator.pop(context, dateTime);
+        },
+      ),
+    );
+
+    if (pickedDate == null || !needToFilter) return;
+
+    // else, call api and refresh
+    int month = pickedDate.month;
+    log('selected month is $month');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppTheme>(
@@ -95,6 +167,18 @@ class _GameHistoryViewState extends State<GameHistoryView>
             titleText: _appScreenText['gameHistory'],
             subTitleText: "${_appScreenText['clubCode']}: ${widget.clubCode}",
             context: context,
+            actionsList: [
+              // button to show filter
+              IconButton(
+                icon: Icon(
+                  Icons.filter_alt_rounded,
+                  color: theme.accentColor,
+                ),
+                onPressed: () {
+                  _showMonthPickerDialog(theme: theme);
+                },
+              ),
+            ],
           ),
           body: _prevGames == null
               ? Center(
