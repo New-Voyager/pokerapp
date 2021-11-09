8e45dd210a8c4bab745d6087bb32f8d64632c1f1
somavoyager <66895802+somavoyager@users.noreply.github.com>
2021-11-09 14:54:42 -0500

    fixed create club dialog keyboard issue; added max length of 500 to the description field of the dialog (#602)
    Co-authored-by: Karun Shrestha <kars.kit35@gmail.com>

--- lib/screens/club_screen/create_club.dart
+++ lib/screens/club_screen/create_club.dart
@@ -5,8 +5,6 @@ import 'package:pokerapp/exceptions/exceptions.dart';
 import 'package:pokerapp/models/ui/app_text.dart';
 import 'package:pokerapp/models/ui/app_theme.dart';
 import 'package:pokerapp/resources/app_decorators.dart';
-import 'package:pokerapp/screens/main_screens/clubs_page_view/widgets/create_club_bottom_sheet.dart';
-import 'package:pokerapp/services/app/club_interior_service.dart';
 import 'package:pokerapp/services/app/clubs_service.dart';
 import 'package:pokerapp/widgets/buttons.dart';
 import 'package:pokerapp/utils/adaptive_sizer.dart';
@@ -28,138 +26,132 @@ class CreateClubDialog {
       context: context,
       builder: (_) {
         final theme = AppTheme.getTheme(context);
-        return Padding(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Center(
-            child: Material(
-              color: Colors.transparent,
-              child: Container(
-                  height: MediaQuery.of(context).size.height * 0.61,
-                  margin: EdgeInsets.all(16),
-                  padding:
-                      EdgeInsets.only(bottom: 24, top: 8, right: 8, left: 8),
-                  decoration: AppDecorators.bgRadialGradient(theme).copyWith(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: theme.accentColor, width: 3),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 15),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              appScreenText['createClub'],
-                              style:
-                                  AppDecorators.getHeadLine3Style(theme: theme),
-                            ),
-                            RoundRectButton(
-                              text: appScreenText['create'],
-                              onTap: () async {
-                                if (!_formKey.currentState.validate()) {
-                                  return;
-                                }
-                                _formKey.currentState.save();
-
-                                // create the club now
-                                log('name: $name description: $description');
-                                /* create a club using the clubDetails */
-                                String clubCode;
-                                try {
-                                  clubCode = await ClubsService.createClub(
-                                    clubDetails['name'],
-                                    clubDetails['description'],
-                                  );
-                                } on GQLException catch (e) {
-                                  await showErrorDialog(
-                                      context, 'Error', gqlErrorText(e));
-                                  return null;
-                                } catch (e) {
-                                  await showErrorDialog(
-                                      context, 'Error', errorText('GENERIC'));
-                                  return null;
-                                } finally {}
-
-                                Navigator.pop(context, clubCode);
-                              },
-                              theme: theme,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 8.0,
-                            vertical: 8.0,
-                          ),
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 8.0,
-                          ),
-                          decoration:
-                              AppDecorators.tileDecorationWithoutBorder(theme),
-                          child: Form(
-                            key: _formKey,
-                            child: SingleChildScrollView(
-                              physics: BouncingScrollPhysics(),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  SizedBox(height: 10.ph),
-                                  /* club name */
-                                  Text(
-                                    appScreenText['name'],
-                                    style: AppDecorators.getSubtitleStyle(
-                                        theme: theme),
-                                  ),
-                                  SizedBox(height: 5.ph),
-                                  CardFormTextField(
-                                    theme: theme,
-                                    elevation: 0.0,
-                                    hintText: appScreenText['nameHint'],
-                                    validator: (String val) =>
-                                        val.trim().isEmpty
-                                            ? appScreenText['provideClubName']
-                                            : null,
-                                    onSaved: (String val) =>
-                                        clubDetails['name'] = val.trim(),
-                                  ),
-                                  SizedBox(height: 5.ph),
-                                  Text(
-                                    appScreenText['description'],
-                                    style: AppDecorators.getSubtitleStyle(
-                                        theme: theme),
-                                  ),
-                                  SizedBox(height: 5.ph),
-                                  CardFormTextField(
-                                    theme: theme,
-                                    elevation: 0.0,
-                                    hintText: appScreenText['descriptionHint'],
-                                    validator: (String val) => val
-                                            .trim()
-                                            .isEmpty
-                                        ? appScreenText['provideDescription']
-                                        : null,
-                                    maxLines: 5,
-                                    onSaved: (String val) =>
-                                        clubDetails['description'] = val.trim(),
-                                  ),
-                                  SizedBox(height: 20.ph),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )),
-            ),
+        return AlertDialog(
+          backgroundColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
+          contentPadding: EdgeInsets.zero,
+          content: Container(
+              height: MediaQuery.of(context).size.height * 0.61,
+              padding: EdgeInsets.only(bottom: 24, top: 8, right: 8, left: 8),
+              decoration: AppDecorators.bgRadialGradient(theme).copyWith(
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: theme.accentColor, width: 3),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          appScreenText['createClub'],
+                          style: AppDecorators.getHeadLine3Style(theme: theme),
+                        ),
+                        RoundRectButton(
+                          text: appScreenText['create'],
+                          onTap: () async {
+                            if (!_formKey.currentState.validate()) {
+                              return;
+                            }
+                            _formKey.currentState.save();
+
+                            // create the club now
+                            log('name: $name description: $description');
+                            /* create a club using the clubDetails */
+                            String clubCode;
+                            try {
+                              clubCode = await ClubsService.createClub(
+                                clubDetails['name'],
+                                clubDetails['description'],
+                              );
+                            } on GQLException catch (e) {
+                              await showErrorDialog(
+                                  context, 'Error', gqlErrorText(e));
+                              return null;
+                            } catch (e) {
+                              await showErrorDialog(
+                                  context, 'Error', errorText('GENERIC'));
+                              return null;
+                            } finally {}
+
+                            Navigator.pop(context, clubCode);
+                          },
+                          theme: theme,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 8.0,
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                      ),
+                      decoration:
+                          AppDecorators.tileDecorationWithoutBorder(theme),
+                      child: Form(
+                        key: _formKey,
+                        child: SingleChildScrollView(
+                          physics: BouncingScrollPhysics(),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SizedBox(height: 10.ph),
+                              /* club name */
+                              Text(
+                                appScreenText['name'],
+                                style: AppDecorators.getSubtitleStyle(
+                                    theme: theme),
+                              ),
+                              SizedBox(height: 5.ph),
+                              CardFormTextField(
+                                theme: theme,
+                                elevation: 0.0,
+                                hintText: appScreenText['nameHint'],
+                                validator: (String val) => val.trim().isEmpty
+                                    ? appScreenText['provideClubName']
+                                    : null,
+                                onSaved: (String val) =>
+                                    clubDetails['name'] = val.trim(),
+                              ),
+                              SizedBox(height: 5.ph),
+                              Text(
+                                appScreenText['description'],
+                                style: AppDecorators.getSubtitleStyle(
+                                    theme: theme),
+                              ),
+                              SizedBox(height: 5.ph),
+                              CardFormTextField(
+                                theme: theme,
+                                elevation: 0.0,
+                                hintText: appScreenText['descriptionHint'],
+                                validator: (String val) => val.trim().isEmpty
+                                    ? appScreenText['provideDescription']
+                                    : null,
+                                maxLines: 5,
+                                maxLength: 500,
+                                onSaved: (String val) =>
+                                    clubDetails['description'] = val.trim(),
+                              ),
+                              SizedBox(height: 20.ph),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
         );
       },
     );
