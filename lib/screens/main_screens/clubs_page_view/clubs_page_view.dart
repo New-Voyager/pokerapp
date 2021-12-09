070c23e5b8858df2d37857c35c03e33e2f5cd3ac
somavoyager <somavoyager@gmail.com>
2021-12-09 10:52:10 -0500

    Working on onboarding

--- lib/screens/main_screens/clubs_page_view/clubs_page_view.dart
+++ lib/screens/main_screens/clubs_page_view/clubs_page_view.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:onboarding_overlay/onboarding_overlay.dart';
 import 'package:pokerapp/exceptions/exceptions.dart';
 import 'package:pokerapp/main.dart';
 import 'package:pokerapp/main_helper.dart';
@@ -21,6 +22,7 @@ import 'package:pokerapp/screens/main_screens/clubs_page_view/widgets/club_item.
 import 'package:pokerapp/screens/main_screens/clubs_page_view/widgets/create_club_bottom_sheet.dart';
 import 'package:pokerapp/services/app/clubs_service.dart';
 import 'package:pokerapp/services/nats/nats.dart';
+import 'package:pokerapp/services/onboarding.dart';
 import 'package:pokerapp/services/test/mock_data.dart';
 import 'package:pokerapp/utils/alerts.dart';
 import 'package:pokerapp/widgets/buttons.dart';
@@ -40,6 +42,7 @@ class _ClubsPageViewState extends State<ClubsPageView>
   @override
   String get routeName => Routes.club_pages;
   bool _showLoading = false;
+  final GlobalKey<OnboardingState> onboardingKey = GlobalKey<OnboardingState>();
 
   List<ClubModel> _clubs;
   AppTextScreen _appScreenText;
@@ -179,6 +182,16 @@ class _ClubsPageViewState extends State<ClubsPageView>
     //   const Duration(seconds: 5),
     //   (_) => _fetchClubs(withLoading: false),
     // );
+
+    WidgetsBinding.instance.addPostFrameCallback((Duration timeStamp) {
+      if (appState.currentIndex != 1) {
+        return;
+      }
+      final OnboardingState onboarding = onboardingKey.currentState;
+      if (onboarding != null) {
+        onboarding.show();
+      }
+    });    
   }
 
   void refreshClubScreen() {
@@ -214,136 +227,239 @@ class _ClubsPageViewState extends State<ClubsPageView>
     super.dispose();
   }
 
+  List<FocusNode> focusNodes = List<FocusNode>.generate(
+      2,
+      (int i) => FocusNode(),
+      growable: false,
+    );
+  Map<int, OnboardingType> onboardOptions = {};
+
+  List<OnboardingStep> getOnboardingSteps(AppTheme appTheme) {
+    List<OnboardingStep> steps = [];
+
+    bool onboardSearchButton = OnboardingService.showSearchClubButton;
+    bool onboardCreateButton = OnboardingService.showCreateClubButton;
+    if (onboardSearchButton) {
+      onboardOptions[steps.length] = OnboardingType.SEARCH_CLUB_BUTTON;
+      steps.add(OnboardingStep(
+        focusNode: focusNodes[0],
+        title: 'Search Club',
+        bodyText: "Tap here to search a club using club code",
+        titleTextColor: Colors.white,
+        labelBoxPadding: const EdgeInsets.all(16.0),
+        labelBoxDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+          color: appTheme.fillInColor,
+          border: Border.all(
+            color: appTheme.secondaryColor,
+            width: 1.0,
+            style: BorderStyle.solid,
+          ),
+        ),
+        arrowPosition: ArrowPosition.top,
+        hasArrow: true,
+        hasLabelBox: true,
+        fullscreen: true,
+      ));
+    }
+
+    if (onboardCreateButton) {
+      onboardOptions[steps.length] = OnboardingType.CREATE_CLUB_BUTTON;
+      steps.add(
+        OnboardingStep(
+          focusNode: focusNodes[1],
+          title: "Create Club",
+          bodyText: "Tap here to create a new club",
+          titleTextColor: Colors.white,
+          labelBoxPadding: const EdgeInsets.all(16.0),
+          labelBoxDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+            color: appTheme.fillInColor,
+            border: Border.all(
+              color: appTheme.secondaryColor,
+              width: 1.0,
+              style: BorderStyle.solid,
+            ),
+          ),
+          arrowPosition: ArrowPosition.top,
+          hasArrow: true,
+          hasLabelBox: true,
+          fullscreen: true,
+        ),
+      );
+    }
+    return steps;
+  }
+
   @override
   Widget build(BuildContext context) {
     Locale locale = Localizations.localeOf(context);
     log('Language code: ${locale.languageCode}');
+    return Consumer<AppTheme>(builder: (_, theme, __) {
+      Widget mainView = buildMainView(theme);
+        List<OnboardingStep> steps = getOnboardingSteps(theme);
+        if (steps.length == 0) {
+          return mainView;
+        }
 
-    return Consumer<AppTheme>(
-      builder: (_, theme, __) => WillPopScope(
-        onWillPop: () async {
-          context.read<ClubsUpdateState>().removeListener(listener);
-          routeObserver.unsubscribe(this);
-          return true;
-        },
-        child: Builder(
-          builder: (ctx) => Container(
-            decoration: AppDecorators.bgRadialGradient(theme),
-            child: Scaffold(
-              backgroundColor: Colors.transparent,
-              body: Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).padding.top,
-                  left: AppDimensions.kMainPaddingHorizontal,
-                  right: AppDimensions.kMainPaddingHorizontal,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    /*
+        Future.delayed(Duration(milliseconds: 500), () {
+          if (appState.currentIndex != 1) {
+            return;
+          }
+          final OnboardingState onboarding = onboardingKey.currentState;
+          if (onboarding != null) {
+            onboarding.show();
+          }
+        });        
+
+        return Onboarding(
+            key: onboardingKey,
+            autoSizeTexts: true,
+            steps: steps,
+            onChanged: (int index) {
+              log('Onboarding: index: $index, type: ${onboardOptions[index]}');
+              if (onboardOptions[index] == OnboardingType.SEARCH_CLUB_BUTTON) {
+                OnboardingService.showSearchClubButton = false;
+              } else if (onboardOptions[index] == OnboardingType.CREATE_CLUB_BUTTON) {
+                OnboardingService.showCreateClubButton = false;
+              }
+            },
+            child: mainView);      
+    });
+  }
+
+  Widget buildMainView(AppTheme theme) {
+    return WillPopScope(
+      onWillPop: () async {
+        context.read<ClubsUpdateState>().removeListener(listener);
+        routeObserver.unsubscribe(this);
+        return true;
+      },
+      child: Builder(
+        builder: (ctx) => Container(
+          decoration: AppDecorators.bgRadialGradient(theme),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top,
+                left: AppDimensions.kMainPaddingHorizontal,
+                right: AppDimensions.kMainPaddingHorizontal,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  /*
                     * title
                     * */
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        RoundRectButton(
-                          text: _appScreenText['searchClub'],
-                          onTap: () async {
-                            await showModalBottomSheet(
-                              context: context,
-                              backgroundColor: Colors.transparent,
-                              isScrollControlled: true,
-                              builder: (ctx) => SearchClubBottomSheet(),
-                            );
-                            _fetchClubs();
-                          },
-                          theme: theme,
-                        ),
-                        HeadingWidget(heading: _appScreenText['clubs']),
-                        RoundRectButton(
-                          text: '+ ${_appScreenText['createClub']}',
-                          onTap: () => _createClub(ctx),
-                          theme: theme,
-                        ),
-                      ],
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RoundRectButton(
+                        text: _appScreenText['searchClub'],
+                        onTap: () async {
+                          await showModalBottomSheet(
+                            context: context,
+                            backgroundColor: Colors.transparent,
+                            isScrollControlled: true,
+                            builder: (ctx) => SearchClubBottomSheet(),
+                          );
+                          _fetchClubs();
+                        },
+                        focusNode: focusNodes[0],
+                        theme: theme,
+                      ),
+                      HeadingWidget(heading: _appScreenText['clubs']),
+                      RoundRectButton(
+                        text: '+ ${_appScreenText['createClub']}',
+                        onTap: () => _createClub(ctx),
+                        theme: theme,
+                        focusNode: focusNodes[1],
+                      ),
+                    ],
+                  ),
 
-                    // _getTitleTextWidget('Clubs'),
+                  // _getTitleTextWidget('Clubs'),
 
-                    // const SizedBox(height: 30),
-                    /*
+                  // const SizedBox(height: 30),
+                  /*
                     * create and search box
                     * */
-                    // const SizedBox(height: 10),
-                    (_showLoading || (_clubs == null))
-                        ? Expanded(
-                            child: Center(
-                              child: CircularProgressWidget(),
-                            ),
-                          )
-                        : _clubs.isEmpty
-                            ? Expanded(
-                                child: Center(
-                                  child: Text(
-                                    _appScreenText['noClubs'],
-                                    style: AppDecorators.getAccentTextStyle(
-                                        theme: theme),
-                                  ),
-                                ),
-                              )
-                            : Expanded(
-                                child: ListView.separated(
-                                  physics: const BouncingScrollPhysics(),
-                                  padding: const EdgeInsets.only(
-                                    bottom: 15.0,
-                                  ),
-                                  itemBuilder: (_, index) {
-                                    var club = _clubs[index];
-                                    return InkWell(
-                                      onTap: () {
-                                        log('Opening club ${club.clubCode}');
-                                        this.openClub(context, club);
-                                      },
-                                      // onLongPress: () => _showClubOptions(
-                                      //   club,
-                                      //   ctx,
-                                      //   theme,
-                                      // ),
-                                      child: Container(
-                                        decoration:
-                                            AppDecorators.getGameItemDecoration(
-                                                theme: theme),
-                                        child: Stack(
-                                          children: [
-                                            ColorFiltered(
-                                              colorFilter: ColorFilter.mode(
-                                                  theme.gameListShadeColor,
-                                                  BlendMode.srcATop),
-                                              child: Image(
-                                                image: AssetImage(
-                                                  AppAssetsNew
-                                                      .pathLiveGameItemBackground,
-                                                ),
-                                                fit: BoxFit.fitWidth,
-                                              ),
-                                            ),
-                                            ClubItem(
-                                              club: club,
-                                              theme: theme,
-                                              appScreenText: _appScreenText,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  separatorBuilder: (_, __) =>
-                                      const SizedBox(height: 16.0),
-                                  itemCount: _clubs.length,
+                  // const SizedBox(height: 10),
+                  (_showLoading || (_clubs == null))
+                      ? Expanded(
+                          child: Center(
+                            child: CircularProgressWidget(),
+                          ),
+                        )
+                      : _clubs.isEmpty
+                          ? Expanded(
+                              child: Center(
+                                child: Text(
+                                  _appScreenText['noClubs'],
+                                  style: AppDecorators.getAccentTextStyle(
+                                      theme: theme),
                                 ),
                               ),
-                  ],
-                ),
+                            )
+                          : Expanded(
+                              child: 
+                              ListView.separated(
+                                physics: const BouncingScrollPhysics(),
+                                padding: const EdgeInsets.only(
+                                  bottom: 15.0,
+                                ),
+                                itemBuilder: (_, index) {
+                                  if (index == 0) {
+                                    return Center(child: Text('Tap a club to go to club screen'),);
+                                  }
+                                  var club = _clubs[index - 1];
+                                  return InkWell(
+                                    onTap: () {
+                                      log('Opening club ${club.clubCode}');
+                                      this.openClub(context, club);
+                                    },
+                                    // onLongPress: () => _showClubOptions(
+                                    //   club,
+                                    //   ctx,
+                                    //   theme,
+                                    // ),
+                                    child: Container(
+                                      decoration:
+                                          AppDecorators.getGameItemDecoration(
+                                              theme: theme),
+                                      child: Stack(
+                                        children: [
+                                          ColorFiltered(
+                                            colorFilter: ColorFilter.mode(
+                                                theme.gameListShadeColor,
+                                                BlendMode.srcATop),
+                                            child: Image(
+                                              image: AssetImage(
+                                                AppAssetsNew
+                                                    .pathLiveGameItemBackground,
+                                              ),
+                                              fit: BoxFit.fitWidth,
+                                            ),
+                                          ),
+                                          ClubItem(
+                                            club: club,
+                                            theme: theme,
+                                            appScreenText: _appScreenText,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                                separatorBuilder: (_, __) =>
+                                    const SizedBox(height: 16.0),
+                                itemCount: _clubs.length+1,
+                              ),
+                            ),
+                ],
               ),
             ),
           ),
