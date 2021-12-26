b012f0f44bd0a07a0ff999bd1de1f17c724d9e26
somavoyager <somavoyager@gmail.com>
2021-12-26 17:13:17 -0500

    fixed incorrect path in prodprep.sh

--- prodprep.sh
+++ prodprep.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp ../devops/pokerapp/GoogleService-Info.plist ios/config/prod/
 cp ../devops/pokerapp/google-services.json android/app/src/prod/
-cp ../devops/pokerapp/upload-keystore.jks android/
+cp ../devops/pokerapp/upload-keystore.jks android/app
