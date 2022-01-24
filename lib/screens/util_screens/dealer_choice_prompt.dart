026b6acaf2a9ba38aa31d14c9cd3403e60b976cc
Karun Shrestha <kars.kit35@gmail.com>
2022-01-25 01:43:06 +0545

    worked on dealer choice prompt ui

--- lib/screens/util_screens/dealer_choice_prompt.dart
+++ lib/screens/util_screens/dealer_choice_prompt.dart
@@ -6,6 +6,7 @@ import 'package:pokerapp/enums/game_type.dart';
 import 'package:pokerapp/main_helper.dart';
 import 'package:pokerapp/models/newmodels/game_model_new.dart';
 import 'package:pokerapp/models/ui/app_theme.dart';
+import 'package:pokerapp/resources/app_decorators.dart';
 import 'package:pokerapp/resources/new/app_colors_new.dart';
 import 'package:pokerapp/resources/new/app_dimenstions_new.dart';
 import 'package:pokerapp/screens/game_play_screen/widgets/milliseconds_counter.dart';
@@ -96,6 +97,9 @@ class _DealerChoicePromptState extends State<DealerChoicePrompt> {
         ),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 8.pw,
+      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -103,10 +107,7 @@ class _DealerChoicePromptState extends State<DealerChoicePrompt> {
             padding: EdgeInsets.all(16),
             child: Text(
               'Choose a game',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-              ),
+              style: AppDecorators.getAccentTextStyle(theme: appTheme),
             ),
           ),
           SwitchWidget2(
@@ -115,44 +116,54 @@ class _DealerChoicePromptState extends State<DealerChoicePrompt> {
               onChange: (v) {
                 doubleBoard = v;
               }),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.7,
-            //height: MediaQuery.of(context).size.height*0.4,
-            child: Wrap(
-              alignment: WrapAlignment.spaceAround,
-              children: List.generate(widget.listOfGameTypes.length, (index) {
-                return ElevatedButton.icon(
-                  style:
-                      ElevatedButton.styleFrom(primary: appTheme.primaryColor),
-                  onPressed: () {
-                    if (this.widget.onSelect != null) {
-                      this
-                          .widget
-                          .onSelect(widget.listOfGameTypes[index], doubleBoard);
-                    }
+          Flexible(
+            child: Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                // height: MediaQuery.of(context).size.height * 0.4,
+                margin: EdgeInsets.only(
+                  top: 8.ph,
+                ),
+                child: GridView.builder(
+                  itemCount: widget.listOfGameTypes.length,
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    return ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        primary: appTheme.primaryColor,
+                        alignment: Alignment.centerLeft,
+                      ),
+                      onPressed: () {
+                        if (this.widget.onSelect != null) {
+                          this.widget.onSelect(
+                              widget.listOfGameTypes[index], doubleBoard);
+                        }
+                      },
+                      icon: Image.asset(
+                          GameModelNew.getGameTypeImageAssetFromEnum(
+                              widget.listOfGameTypes[index]),
+                          width: 24,
+                          height: 24),
+                      label: Text(
+                        "${gameTypeShortStr(widget.listOfGameTypes[index])}",
+                        //textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 10.dp,
+                          color: AppColorsNew.newGreenButtonColor,
+                          height: 0.9,
+                        ),
+                      ),
+                    );
                   },
-                  icon: Image.asset(
-                      GameModelNew.getGameTypeImageAssetFromEnum(
-                          widget.listOfGameTypes[index]),
-                      width: 24,
-                      height: 24),
-                  label: Text(
-                    "${gameTypeShortStr(widget.listOfGameTypes[index])}",
-                    //textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 10.dp,
-                      color: AppColorsNew.newGreenButtonColor,
-                    ),
-                  ),
-                );
-              }),
-            ),
+                )),
           ),
           Divider(
             color: AppColorsNew.yellowAccentColor,
-            height: 8,
-            endIndent: 16,
-            indent: 16,
+            height: 32,
           ),
           ElevatedButton(
             style: ElevatedButton.styleFrom(primary: appTheme.primaryColor),
