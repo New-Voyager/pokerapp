1d1436b7c848dac7b72e9301e40d98696fd59856
soma <soma.voyager@gmail.com>
2022-06-18 01:16:50 -0400

    Enabled bot game for testing

--- lib/screens/game_screens/new_game_settings/new_game_settings2.dart
+++ lib/screens/game_screens/new_game_settings/new_game_settings2.dart
@@ -1323,14 +1323,14 @@ class _NewGameSettings2State extends State<NewGameSettings2> {
 
                               /* sep */
                               NewGameSettings2.sepV8,
-                              // _buildRadio(
-                              //   label: appScreenText['botGame'],
-                              //   value: gmp.botGame,
-                              //   onChange: (bool b) {
-                              //     gmp.botGame = b;
-                              //   },
-                              //   theme: theme,
-                              // ),
+                              _buildRadio(
+                                label: appScreenText['botGame'],
+                                value: gmp.botGame,
+                                onChange: (bool b) {
+                                  gmp.botGame = b;
+                                },
+                                theme: theme,
+                              ),
                               /* location check */
                               _buildRadio(
                                 label: appScreenText['locationCheck'],
